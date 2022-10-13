353234df3c9199285b4d8ac556de18d4a516e06a
cxnt <pavelbartsits@gmail.com>
2022-10-13 19:10:49 +0400

    import from agent

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -1,10 +1,8 @@
 import os
 import sys
-from distutils.util import strtobool
 from pathlib import Path
 
 import supervisely as sly
-from dotenv import load_dotenv
 from supervisely.app.v1.app_service import AppService
 from supervisely.io.fs import mkdir
 
@@ -13,10 +11,11 @@ print(f"App source directory: {root_source_dir}")
 sys.path.append(root_source_dir)
 
 # only for convenient debug
-# debug_env_path = os.path.join(root_source_dir, "debug.env")
-# secret_debug_env_path = os.path.join(root_source_dir, "secret_debug.env")
-# load_dotenv(debug_env_path)
-# load_dotenv(secret_debug_env_path, override=True)
+from dotenv import load_dotenv
+debug_env_path = os.path.join(root_source_dir, "debug.env")
+secret_debug_env_path = os.path.join(root_source_dir, "secret_debug.env")
+load_dotenv(debug_env_path)
+load_dotenv(secret_debug_env_path, override=True)
 
 
 api: sly.Api = sly.Api.from_env()
@@ -28,6 +27,11 @@ TASK_ID = int(os.environ["TASK_ID"])
 
 INPUT_DIR: str = os.environ.get("modal.state.slyFolder", None)
 INPUT_FILE: str = os.environ.get("modal.state.slyFile", None)
+
+if INPUT_DIR:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_DIR)
+else:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_FILE)
 
 STORAGE_DIR: str = my_app.data_dir
 mkdir(STORAGE_DIR, True)
